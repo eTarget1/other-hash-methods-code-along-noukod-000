a99f9dbb3981166e98614f07d20f18dbe238e01b
clifford <clifford@noukod.com>
2018-09-25 16:27:18 +0000

    automatically backed up by learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -7,7 +7,12 @@ require 'pry'
 #  meat: ["chicken", "steak", "salmon"],
 #  grains: ["rice", "pasta"]
 # }
-groceries = {fruit: "Banana", vegetable: "Broccoli", dessert: "Cookie"}
+groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
 def get_the_keys(groceries)
   #code your solution here!
   groceries.keys
