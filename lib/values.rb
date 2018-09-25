7631cb0faed7d3ed978fcbe2b60d50a340de9538
clifford <clifford@noukod.com>
2018-09-25 16:27:26 +0000

    automatically backed up by learn

--- lib/values.rb
+++ lib/values.rb
@@ -8,8 +8,12 @@ require 'pry'
 #  grains: ["rice", "pasta"]
 # }
 
-groceries = {fruit: "Banana", vegetable: "Broccoli", dessert: "Cookie"}
-
+groceries = {
+  dairy: ["milk", "yogurt", "cheese"],
+  vegetable: ["carrots", "broccoli", "cucumbers"],
+  meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+}
 def get_the_values(groceries)
   #code your solution here!
 groceries.values
